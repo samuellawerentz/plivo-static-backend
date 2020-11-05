73ced808d7754add4ab02242a34bbf250320f0ad
Samuel Lawerence J <samuellawrentz@gmail.com>
2020-11-05 12:43:51 +0530

    Updated function name

--- validation/enrich.js
+++ validation/enrich.js
@@ -1,8 +1,8 @@
 // Simple Contact Form Spam Filter
 const axios = require('axios').default;
 exports.handler = async function(event, context) {
-
-    const res = await axios.get('https://li.plivo.sh/v2/person/?email=samuel.lawerence@plivo.com', {
+    const email = event.queryStringParameters.email
+    const res = await axios.get('https://li.plivo.sh/v2/person/?email=' + email, {
         headers: {
             'Authorization': 'Basic bWFya2V0aW5nb3BzOm1vZG93bER0Snp4MzE='
           }
